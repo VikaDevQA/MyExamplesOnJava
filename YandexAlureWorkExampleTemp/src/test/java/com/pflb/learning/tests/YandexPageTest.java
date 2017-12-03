27955f0fefe6338cbf005e5b836ca74531c71cb2
Vika <tempvika@bk.ru>
2017-12-03 23:35:03 +0400

    add changes into project

--- YandexAlureWorkExampleTemp/src/test/java/com/pflb/learning/tests/YandexPageTest.java
+++ YandexAlureWorkExampleTemp/src/test/java/com/pflb/learning/tests/YandexPageTest.java
@@ -3,15 +3,20 @@ package com.pflb.learning.tests;
 import com.pflb.learning.pages.YandexMainPage;
 import com.pflb.learning.pages.YandexMarketPage;
 import com.pflb.learning.utils.DriverManager;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Step;
 import io.qameta.allure.junit4.DisplayName;
 import org.junit.AfterClass;
 import org.junit.Test;
-import io.qameta.allure.*;
+import ru.yandex.qatools.allure.annotations.Attachment;
 import ru.yandex.qatools.allure.annotations.Title;
+
 
 import java.util.NoSuchElementException;
 
+@Title("Go to Market Page")
 public class YandexPageTest {
     YandexMainPage yandexMainPage;
     YandexMarketPage yandexMarketPage;
@@ -19,18 +24,15 @@ public class YandexPageTest {
     /**
      * Переход на вкладку Ноутбуки в разделе Маркет
      */
-    //@Step
-    //@Title("Go to Market Page")
-    //@Description("Go to Market Page")
-    //@DisplayName("Go to Market Page")
     @Test
-    //@DisplayName("Go to Market Page")
+    @DisplayName("Go to Market Page")
+    //@Step("Type {yandexMainPage.market}}")
+    @Step("Click link 'Маркет'")
+    @Severity(SeverityLevel.NORMAL)
     public void successGoToNotebooks() throws Exception {
         yandexMainPage = new YandexMainPage();
         //yandexMainPage.goToMarket();
-
         try {
-
             yandexMarketPage = yandexMainPage.goToMarket();
         } catch (NullPointerException e) {
             System.out.println("Экзмепляр класса,который вызывает метод не проинициализирован");
