04af7e35eb111a998e2791fbd9e951e159406931
Vika <tempvika@bk.ru>
2017-12-03 22:36:38 +0400

    add FirstYandexAllureReport

---
+++ YandexAlureWorkExampleTemp/src/test/java/com/pflb/learning/tests/YandexPageTest.java
@@ -0,0 +1,48 @@
+package com.pflb.learning.tests;
+
+import com.pflb.learning.pages.YandexMainPage;
+import com.pflb.learning.pages.YandexMarketPage;
+import com.pflb.learning.utils.DriverManager;
+import io.qameta.allure.Step;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.AfterClass;
+import org.junit.Test;
+import io.qameta.allure.*;
+import ru.yandex.qatools.allure.annotations.Title;
+
+import java.util.NoSuchElementException;
+
+public class YandexPageTest {
+    YandexMainPage yandexMainPage;
+    YandexMarketPage yandexMarketPage;
+
+    /**
+     * Переход на вкладку Ноутбуки в разделе Маркет
+     */
+    //@Step
+    //@Title("Go to Market Page")
+    //@Description("Go to Market Page")
+    //@DisplayName("Go to Market Page")
+    @Test
+    //@DisplayName("Go to Market Page")
+    public void successGoToNotebooks() throws Exception {
+        yandexMainPage = new YandexMainPage();
+        //yandexMainPage.goToMarket();
+
+        try {
+
+            yandexMarketPage = yandexMainPage.goToMarket();
+        } catch (NullPointerException e) {
+            System.out.println("Экзмепляр класса,который вызывает метод не проинициализирован");
+        } catch (NoSuchElementException e) {
+            System.out.println("Элмент с заданным селектором на странице не найден");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterClass //Аннотация Junit. Говорит, что метод должен запускаться каждый раз после всех тестов
+    public static void tearDown() {
+        DriverManager.getDriver().quit();
+    }
+}
